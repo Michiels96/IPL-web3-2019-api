005744bbc94245971efa560b1859fe79cf902173
Raphael <raphi.baroni@gmail.com>
2019-10-31 20:05:05 +0100

    preparation for the recap exercice of week_7

--- modules/server.js
+++ modules/server.js
@@ -30,9 +30,31 @@ const app = express()
 // limit : it controls the maximum request body size. 
 app.use(bodyParser.json({limit:"1.1MB"}));
 
+// Create an authorization middleware to be used on the route to be secured
+var jwt = require('jsonwebtoken');
+const jwtSecret = process.env.JWT_SECRET;
+const authMiddleware = (req, res, next) => {
+    var token = req.get('authorization');
+      if (!token) {          
+          return res.json({ success: false, error: "A token is mandatory to subscribe to this API." });
+      }
+      jwt.verify(token, jwtSecret, (err, decoded) => {
+          if (err) {              
+              return res.json({ success: false, error: "Unable to parse token." });
+          }
+          if (decoded.exp <= Date.now()) {              
+              return res.json({ success: false, error: "Token has expired." });
+          }
+      req.token = decoded;
+      next();
+      });
+  }
+
+
 // Configure routes
 app.use('/users', usersRouter)
-app.use('/gallery', galleryRouter)
+// Secure the Gallery API
+app.use('/gallery',authMiddleware, galleryRouter)
 app.use('/quotes', quotesRouter)
 
 // Start server
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../modules/db')
+const jwt = require("jsonwebtoken");
 
 
 
@@ -21,6 +22,66 @@ router.get("/", function(req, res) {
       res.json(user);
     });
 });
+
+
+router.post("/me", function(req, res, next) {
+  db.db
+    .collection("users")
+    .findOne({ login: req.body.username })
+    .then(user => {
+      console.log(user);
+      if (user) {
+        //compare password. Improve this later with hashed password!
+        if(user.password === req.body.password)
+          {          
+          res.json({ success: true, user: user });
+          }
+        else{
+          console.log("bad password");
+          res.json({ success: false, error: "bad email/password" });
+        }
+     
+      } else {
+        console.log("no user");
+        res.json({ success: false, error: "bad email/password" });
+      }
+    });
+});
+
+// To use it in Production, don't forget to set-up a new environment variable
+const jwtSecret = process.env.JWT_SECRET;
+
+router.post("/me2", function(req, res, next) {
+  db.db
+    .collection("users")
+    .findOne({ login: req.body.username })
+    .then(user => {      
+      if (user) {
+        //compare password. Improve this later with hashed password!
+        if(user.password === req.body.password)
+          {          
+          const exp = Date.now() + 12 * 60 * 60 * 1000; // 12h
+          jwt.sign({ user: user._id, exp: exp }, jwtSecret, (err, token) => {
+            if (err) {
+              console.log("Error at sign()",err);
+              res.json({ success: false, error: "error during token signing" });
+            } else {              
+              res.json({ success: true, user: user,token:token });
+            }
+          });         
+          }
+        else{
+          console.log("bad password");
+          res.json({ success: false, error: "bad email/password" });
+        }
+     
+      } else {
+        console.log("no user");
+        res.json({ success: false, error: "bad email/password" });
+      }
+    });
+});
+
 
 /**
  * Exports
